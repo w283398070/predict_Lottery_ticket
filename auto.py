ce450e79db8746fd42f2a5f998904aa6d22eac03
yuekunwang <yuekunwang@tencent.com>
2023-04-13 10:47:48 +0800

    op

--- auto.py
+++ auto.py
@@ -1,9 +1,17 @@
 # -*- coding:utf-8 -*-
 import os
-import datetime
+import datetime, shutil
 import pandas as pd
 from config import *
+from common import get_current_number
 
+
+def clean_modeling_data():
+    dlt_model_path = os.path.join(model_path, 'dlt')
+    if os.path.exists(dlt_model_path):
+        shutil.rmtree(dlt_model_path)
+    os.makedirs(dlt_model_path)
+    return
 
 def read_predict_data():
     filename = datetime.datetime.now().strftime('%Y%m%d')
@@ -14,26 +22,32 @@ def read_predict_data():
     data = pd.read_csv(fileadd)
     print(data)
     return data.values.tolist()
-def predict_total(predict_list):
-    filename = datetime.datetime.now().strftime('%Y%m%d')
+def predict_total(predict_list, win):
     filepath = "{}{}/".format(predict_path, "dlt")
-    fileadd = "{}{}_{}{}".format(filepath, 'predict', filename, ".csv")
+    fileadd = "{}{}_{}_{}{}".format(filepath, 'predict', get_current_number("dlt"), win, ".csv")
     if not os.path.exists(filepath):
         os.makedirs(filepath)
     df = pd.DataFrame(predict_list, columns=['red1', 'red2', 'red3', 'red4', 'red5', 'blue1', 'blue2'])
     df.to_csv(fileadd, encoding="utf-8", mode='a', header=False, index=False)
 
-def train_and_predict():
+def train_and_predict(train_count):
     print('train_and_predict start')
-    os.system('python run_train_model.py --name dlt  --windows_size 3,5,7 --red_epochs 100 --blue_epochs 100 --batch_size 1')
+    #os.system('python run_train_model.py --name dlt  --windows_size 3,5,7 --red_epochs 666 --blue_epochs 666 --batch_size 1')
+    train_cmd = "{}{}{}{}{}".format("python run_train_model.py --name dlt  --windows_size 3,5,7 --red_epochs ", train_count, " --blue_epochs ", train_count, " --batch_size 1")
+    os.system(train_cmd)
     os.system('python run_predict.py  --name dlt --windows_size 3,5,7')
     print('train_and_predict end')
 
 print('start')
 count = 0
+train_count = 1
 while(count < 1000):
-    train_and_predict()
-    predict_total(read_predict_data())
+    train_and_predict(train_count)
+    predict_data_list = read_predict_data()
+    predict_total([predict_data_list[0]], 3)
+    predict_total([predict_data_list[1]], 5)
+    predict_total([predict_data_list[2]], 7)
     count += 1
+    train_count += 1
 
 print('end')
--- dlt_tar.py
+++ dlt_tar.py
@@ -3,11 +3,13 @@ import os
 import datetime
 import pandas as pd
 from config import *
+from common import get_current_number
 
-def read_predict_data():
-    filename = datetime.datetime.now().strftime('%Y%m%d')
+def read_predict_data(win):
     filepath = "{}{}/".format(predict_path, "dlt")
-    fileadd = "{}{}_{}{}".format(filepath, 'predict', filename, ".csv")
+    #fileadd = "{}{}_{}{}".format(filepath, 'predict', filename, ".csv")
+    fileadd = "{}{}_{}_{}{}".format(filepath, 'predict', get_current_number("dlt"), win, ".csv")
+    print(fileadd)
     if not os.path.exists(fileadd):
         return [],[]
     data = pd.read_csv(fileadd)
@@ -16,12 +18,16 @@ def read_predict_data():
 
 def matching_degree(src_list, dst_list):
     src_red = src_list[:5]
-    src_blue = src_list[:-2]
+    src_blue = src_list[5:]
     dst_red = dst_list[:5]
-    dst_blue = dst_list[:-2]
+    dst_blue = dst_list[5:]
     src_set = set(src_red)
     dst_set = set(dst_red)
-    count = len(src_set.intersection(dst_set))
+    intersect = src_set.intersection(dst_set)
+    #print(intersect)
+    count = len(intersect)
+    #print(src_blue)
+    #print(dst_blue)
     if src_blue[0] in dst_blue:
         count += 1
     if src_blue[1] in dst_blue:
@@ -30,14 +36,18 @@ def matching_degree(src_list, dst_list):
 
 
 def cacl_match(data_list):
-    tar_list = [2, 24, 26, 30, 34, 6, 7]
+    tar_list = [4,5,18,22,30,5,12]
     for sublist in data_list:
         degree = matching_degree(sublist, tar_list)
-        if degree >= 3:
+        if degree >= 4:
             print('count:' + str(degree))
             print(sublist)
 
 print('start')
-data_list = read_predict_data()
+data_list = read_predict_data(3)
+cacl_match(data_list)
+data_list = read_predict_data(5)
+cacl_match(data_list)
+data_list = read_predict_data(7)
 cacl_match(data_list)
 print('end')
